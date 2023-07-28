23c39c07ec9ba19af2768b4326490eb98e648f76
Matias Hamie <matias@bytandym.com>
2023-07-28 13:41:24 -0300

    feat(mApps): Integrated scrips within code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* 
+          -- One way to handle script --
+          <script
+            defer
+            type="text/javascript"
+            src="https://assets.platform.bytandym.com/custom-mapps-assets/tandym-mapps-custom-v1.js?tandym_api_key=185c732e-02f0-4b01-bd57-812d70992a62-42485d6c-c010-42a0-bbc3-bfc650acc791-1689284239728"
+          ></script>
+
+         -- Second way to handle the script --
+          <Script
+            src="https://assets.platform.bytandym.com/custom-mapps-assets/tandym-mapps-custom-v1.js?tandym_api_key=185c732e-02f0-4b01-bd57-812d70992a62-42485d6c-c010-42a0-bbc3-bfc650acc791-1689284239728"
+            strategy="lazyOnload"
+          /> 
+        */}
+      </Head>
       <body>
         <Main />
         <NextScript />
+
+        {/* Third way to handle the script */}
+        {/* <Script
+          src="https://assets.platform.bytandym.com/custom-mapps-assets/tandym-mapps-custom-v1.js?tandym_api_key=185c732e-02f0-4b01-bd57-812d70992a62-42485d6c-c010-42a0-bbc3-bfc650acc791-1689284239728"
+          strategy="lazyOnload"
+        /> */}
       </body>
     </Html>
-  )
+  );
 }
